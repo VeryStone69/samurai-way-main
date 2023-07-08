8d54666088b5078615c823c3a830e33adfdbb59a
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-07-08 09:35:55 +0200

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello, samurai! Let's go! TEST TEST TEST
+        </div>
+    );
 }
 
 export default App;
