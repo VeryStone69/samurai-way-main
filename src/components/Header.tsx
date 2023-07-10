588d3e6d0c4a46cb3cab4a0e026c52b4778be73e
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-07-10 11:21:48 +0200

    sprint 1: samurai-way (lesson 14,style module)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import s from "./Header.module.css";
 export const Header = () => {
     return (
-        <header className={"header"}>
+        <header className={s.header}>
             <div>Header</div>
             <img src="https://img.freepik.com/free-icon/diamond_318-195445.jpg?size=626&ext=jpg" alt="logo"/>
         </header>
