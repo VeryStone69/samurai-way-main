1d9c4419de0bf415b92322abb1cd4fa1d4d02acf
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-07-20 15:04:34 +0200

    sprint 2: samurai-way (lesson 21, creating styles for Dialogs)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,14 +1,32 @@
 import React from "react"
 import s from "./Dialogs.module.css"
 
-type DialogsPropsType = {
+type DialogsPropsType = {}
 
-}
-
-export const Dialogs:React.FC<DialogsPropsType> = (props) =>{
+export const Dialogs: React.FC<DialogsPropsType> = (props) => {
     return (
-        <div>
-            DIALOGS
+        <div className={s.wrapperDialogs}>
+            <div className={s.dialogs}>
+                <div className={s.dialog + " " + s.active}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    Alex1
+                </div>
+                <div className={s.dialog}>
+                    Alex2
+                </div>
+                <div className={s.dialog}>
+                    Alex2
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Message1</div>
+                <div className={s.message}>Message2</div>
+                <div className={s.message}>Message3</div>
+            </div>
         </div>
+
+
     )
 }
