50f438f50aeb9f2429a454092ad8873e0b7f2a3b
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-07-20 11:41:07 +0200

    sprint 2: samurai-way (lesson 19, route)

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,26 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route,Routes} from "react-router-dom";
 
 
 export const App = () => {
     return (
+
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
             <div className="app-wrapper-content">
-                <Dialogs/>
+                <Routes>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/dialogs" element={<Dialogs/>}/>
+                    {/*<Route path="/news" element={<News/>}/>*/}
+                    {/*<Route path="/music" element={<Music/>}/>*/}
+                    {/*<Route path="/settings" element={<Settings/>}/>*/}
+                </Routes>
             </div>
-            {/*<Profile/>*/}
         </div>
+
 
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,10 +4,11 @@ import s from "./Navbar.module.css"
 export const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={s.item}><a>Profile</a></div>
-            <div className={s.item}><a>Messages</a></div>
-            <div className={s.item}><a>News</a></div>
-            <div className={s.item}><a>Music</a></div>
+            <div className={s.item}><a href='/profile'>Profile</a></div>
+            <div className={s.item}><a href='/dialogs'>Messages</a></div>
+            <div className={s.item}><a href='/news'>News</a></div>
+            <div className={s.item}><a href='/music'>Music</a></div>
+            <div className={s.item}><a href='/settings'>Settings</a></div>
         </nav>
     );
 };
