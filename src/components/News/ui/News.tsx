19727be16f5c25a2174104868a2c4afd1a708d77
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2024-01-14 16:12:20 +0100

    add: news categories, styles for categories

--- src/components/News/ui/News.tsx
+++ src/components/News/ui/News.tsx
@@ -1,26 +1,42 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch} from "react-redux";
 import {getNewsTC, SourcesType} from "../model/news-reducer";
 import {NewsCard} from "../NewsCard/ui/NewsCard";
-import {AppRootStateType, useAppSelector} from "../../../redux/redux-store";
+import {useAppSelector} from "../../../redux/redux-store";
 import {fetchSelector} from "../../Users/selectors/usersContainer-selector";
 import {UsersLoader} from "../../common/UsersLoader";
+import {newsSelector} from "../model/selectors/news-selector";
+import s from "./News.module.css"
 
 export const News = () => {
-    const newsSelector = ((state:AppRootStateType)=>state.news.sources)
+    const categoryNews = ["Technology", "Business", "Health", "Science"];
+    const [currentCategory, setCurrentCategory] = useState(categoryNews[0])
     const news = useAppSelector(newsSelector)
     const fetch = useAppSelector(fetchSelector);
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(getNewsTC())
+        dispatch(getNewsTC(currentCategory))
     }, []);
+    const changeCategoryHandler = (name: string) => {
+        dispatch(getNewsTC(name))
+        setCurrentCategory(name)
+    }
+
     return (
         <>
             {fetch.isFetching
                 ? <UsersLoader/>
                 : <>
-                    {news?.map((el:SourcesType)=>{
-                        return <div><NewsCard key={el.id} data={el}/></div>
+                    <div className={s.buttonCategoryContainer}>
+                        {categoryNews.map((nameCategory: string) => {
+                            return <button
+                                className={currentCategory === nameCategory ? s.categorySelected : s.buttonChangeCategory}
+                                key={nameCategory}
+                                onClick={() => changeCategoryHandler(nameCategory)}>{nameCategory}</button>
+                        })}
+                    </div>
+                    {news?.map((el: SourcesType) => {
+                        return <div key={el.id}><NewsCard data={el}/></div>
                     })}
                 </>
             }
