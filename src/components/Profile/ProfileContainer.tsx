9ed20a068eceba89d96051b9156413e219e2a8a8
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2024-01-17 22:55:16 +0100

    debugging useEffect in ProfileContainer.tsx

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -5,25 +5,25 @@ import {Navigate, useParams} from "react-router-dom";
 import {getProfileDataTC} from "../../redux/users-reducer";
 import {useAppSelector} from "../../redux/redux-store";
 import {getProfileStatusTC} from "../../redux/profile-reducer";
-import {authorizedUserIdSelector, isAuthSelector} from "./selectors/profileContainer-selector";
+import {isAuthSelector} from "./selectors/profileContainer-selector";
 
 
 export const ProfileContainer = () => {
     let params = useParams();
-    let userId = Number(params.userId);
-    const authorizedUserId: any = useAppSelector(authorizedUserIdSelector)
+    let userId = Number(params.userId)
+    let userIdFromLocal = Number(localStorage.getItem('userId'));
+
     const isAuth = useAppSelector(isAuthSelector)
     const dispatch = useDispatch()
+ console.log(!userId)
     useEffect(() => {
-        if (!params.userId) {
-            if (!authorizedUserId !== undefined) {
-                userId = authorizedUserId
-            }
+        if (!userId) {
+            userId = userIdFromLocal
         }
-        dispatch(getProfileDataTC(userId))
-        dispatch(getProfileStatusTC(userId))
-    }, [isAuth,params,authorizedUserId])
-    return isAuth ? <Profile isOwner = {!userId}/> : <Navigate to={"/login"}/>
-}
+        dispatch(getProfileDataTC(userId));
+        dispatch(getProfileStatusTC(userId));
 
+    }, [isAuth, userId, userIdFromLocal])
+    return isAuth ? <Profile isOwner={!userId}/> : <Navigate to={"/login"}/>
+}
 
--- src/components/Profile/selectors/profileContainer-selector.ts
+++ src/components/Profile/selectors/profileContainer-selector.ts
@@ -1,4 +1,3 @@
 import {AppRootStateType} from "../../../redux/redux-store";
 
-export const authorizedUserIdSelector = ((state:AppRootStateType) => state.profile.profile?.userId);
 export const isAuthSelector = ((state:AppRootStateType) => state.auth.isAuth);
