8cc16fa223d36f2f23c844cab5803b92269e02c8
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2024-02-04 15:15:24 +0100

    update: body background

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import {store} from "./redux/redux-store";
 import {App} from "./App";
 import ReactDOM from "react-dom";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
     ReactDOM.render(
